fec4485fedbeed402ce57b2b75c07aa548eb0d46
Rene Arturo Bareño Campos <rene.bareno@gmail.com>
2018-11-19 21:41:37 -0500

    Updates

---
+++ precache-manifest.82cbb59d1214cfc8301df3baf210cead.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ffafa7e513e3e5e60d2",
+    "url": "/Merging-LinkedIn-TorreBio/static/js/runtime~main.9ffafa7e.js"
+  },
+  {
+    "revision": "485fd64bf68bf78c6079",
+    "url": "/Merging-LinkedIn-TorreBio/static/js/main.485fd64b.chunk.js"
+  },
+  {
+    "revision": "989fc44686ee61408302",
+    "url": "/Merging-LinkedIn-TorreBio/static/js/1.989fc446.chunk.js"
+  },
+  {
+    "revision": "485fd64bf68bf78c6079",
+    "url": "/Merging-LinkedIn-TorreBio/static/css/main.f183dffc.chunk.css"
+  },
+  {
+    "revision": "92e157284ebc1c083e072f4230a6aa71",
+    "url": "/Merging-LinkedIn-TorreBio/index.html"
+  }
+];
